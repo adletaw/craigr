0b27714e800c309556b75d3807304ad56e4a2352
Alathea <alathea@zoology.ubc.ca>
2016-03-24 21:49:23 -0700

    added optional parameters

--- R/list_housing.R
+++ R/list_housing.R
@@ -5,6 +5,12 @@
 #'
 #' @param city Character string containing the city you want to look in. Default
 #' is "Seattle".
+#' @param key Optional character string containing a search term to filter
+#' results.
+#' @param price_min Optional numeric containing minimum price.
+#' @param price_max Optional numeric containing maximum price.
+#' @param sqft_min Optional numeric containing minimum square footage.
+#' @param sqft_max Optional numeric containing maximum square footage.
 #'
 #' @return A captioner function.
 #'
@@ -12,6 +18,7 @@
 #'
 #' @export
 
-list_housing <- function(){
+list_housing <- function(city = "Seattle", key = NA, price_min = NA,
+                         price_max = NA, sqft_min = NA, sqft_max = NA){
 
 }
