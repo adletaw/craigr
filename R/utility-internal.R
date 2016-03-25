6bb3b647df49b1e0491207fe8b906db25d7ebd97
Alathea <alathea@zoology.ubc.ca>
2016-03-25 16:49:49 -0700

    added internal utility function, check_class

---
+++ R/utility-internal.R
@@ -0,0 +1,15 @@
+#' Check parameter class
+#'
+#' @description Check the class of a supplied parameter to make sure it matches
+#' expectations.
+#'
+#' @keywords internal
+#' @export
+#'
+check_class <- function(param, expect_class){
+  if(class(param)!=expect_class){
+    stop(paste(param, "must be of class", expect_class, sep = " "))
+  } else{
+    return(invisible)
+  }
+}
