347cb3cbe20f1c52e72637b0cc3f586dadd64ff5
Alathea <alathea@zoology.ubc.ca>
2016-03-24 21:54:58 -0700

    added subdomain parameter to specify which craigslist location should be used

--- R/connect.R
+++ R/connect.R
@@ -1,9 +1,14 @@
 #' Connect to Craigslist
 #'
-#' @description Initialize the connection with Craigslist
+#'@description Initialize the connection with Craigslist
+#'
+#'@param subdomain Character string specifying which Craigslist location you
+#'want to use.  This is used as the subdomain, e.g. "seattle.craigslist.org"
 #'
 #' @export
 
-connect <- function(){
+connect <- function(subdomain = "seattle")
+{
+  url <- paste0("https://", subdomain, ".craigslist.org")
 
 }
