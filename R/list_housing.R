3752ebe038aba496f0a8317eddb619e26e590aef
Alathea <alathea@zoology.ubc.ca>
2016-03-24 21:44:42 -0700

    initialized function to list available housing

---
+++ R/list_housing.R
@@ -0,0 +1,17 @@
+#' List housing
+#'
+#' @description Get a list of housing available on craiglist using your own
+#' specifications
+#'
+#' @param city Character string containing the city you want to look in. Default
+#' is "Seattle".
+#'
+#' @return A captioner function.
+#'
+#' @examples
+#'
+#' @export
+
+list_housing <- function(){
+
+}
