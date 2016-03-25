08d3206f8495c9736830c91194569953cf0463fa
Alathea <alathea@zoology.ubc.ca>
2016-03-25 12:37:52 -0700

    parsed out title, price, date, url

--- R/get_query-internal.R
+++ R/get_query-internal.R
@@ -14,9 +14,40 @@
 get_query <- function(query, type = "apa")
 {
   require(rvest)
+
   ## The raw query
   raw_query <- read_html(query)
 
-  ## Remove everything before the first house listing
-  return(raw_query)
+  ## Select out the apartment ads
+  raw_ads <- html_nodes(raw_query, "span.txt")
+
+  # ## Get the titles
+  titles_raw   <- html_nodes(raw_ads, "span#titletextonly")
+  titles_clean <- html_text(titles_raw)
+
+  ## Get the prices
+  prices_raw   <- html_nodes(raw_ads, "span.price")
+  prices_clean <- html_text(prices_raw)
+
+  ## Get the post date
+  dates_raw   <- html_nodes(raw_ads, "time")
+  dates_clean <- c()
+  for(i in 1:length(dates_raw)){
+    dates_clean[i] <- html_attr(dates_raw[i], "datetime")
+  }
+
+  ## Get the url
+  urls_raw   <- html_nodes(raw_ads, "a")
+  urls_clean <- c()
+  for(i in 1:length(urls_raw)){
+    urls_clean[i] <- html_attr(urls_raw[i], "href")
+  }
+
+  ## Bind the data
+  clean_data <- data.frame(Title = titles_clean,
+                           Price = prices_clean,
+                           Date  = dates_clean,
+                           URL   = urls_clean)
+
+  return(clean_data)
 }
