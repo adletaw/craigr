1b2d1641629fe75d98300056f1d1b2bc35544333
Alathea <alathea@zoology.ubc.ca>
2016-03-27 12:37:43 -0700

    Removed dictionary and internal data (#12)

--- dictionary/generate_package_data.R
+++
@@ -1,6 +0,0 @@
-## Generate the Craigslist dictionary data for use in R package
-
-craigs_places <- read.csv("dictionary/places.csv", stringsAsFactors = FALSE)
-
-## Write to system data
-devtools::use_data(craigs_places, internal = TRUE, overwrite = TRUE)
